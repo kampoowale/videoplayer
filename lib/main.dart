9269fba9e5afea48ae1cbfdde37888c64cd02094
kampoowale <kampoowale@gmail.com>
2020-08-06 22:35:31 +0530

    added dependency

--- lib/main.dart
+++ lib/main.dart
@@ -42,27 +42,27 @@ class MyHomePage extends StatelessWidget {
 //          ),
           ChewieListItem(
             videoPlayerController: VideoPlayerController.network(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+              'https://www.pinkvilla.com/feed/video-test/1GzPt1E-wXaMF_rAmm_E18XdTqqwVocde.mp4',
             ),
           ),
           ChewieListItem(
             videoPlayerController: VideoPlayerController.network(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+              'https://www.pinkvilla.com/feed/video-test/1GzPt1E-wXaMF_rAmm_E18XdTqqwVocde.mp4',
             ),
           ),
           ChewieListItem(
             videoPlayerController: VideoPlayerController.network(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+              'https://www.pinkvilla.com/feed/video-test/1GzPt1E-wXaMF_rAmm_E18XdTqqwVocde.mp4',
             ),
           ),
           ChewieListItem(
             videoPlayerController: VideoPlayerController.network(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+              'https://www.pinkvilla.com/feed/video-test/1GzPt1E-wXaMF_rAmm_E18XdTqqwVocde.mp4',
             ),
           ),
           ChewieListItem(
             videoPlayerController: VideoPlayerController.network(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+              'https://www.pinkvilla.com/feed/video-test/1GzPt1E-wXaMF_rAmm_E18XdTqqwVocde.mp4',
             ),
           ),
 //          ChewieListItem(
@@ -100,26 +100,88 @@ class _ChewieListItemState extends State<ChewieListItem> {
   @override
   void initState() {
     super.initState();
-    // Wrapper on top of the videoPlayerController
+//     Wrapper on top of the videoPlayerController
+//    _chewieController = ChewieController(
+//      videoPlayerController: widget.videoPlayerController,
+//      aspectRatio: 16 / 9,
+//      // Prepare the video to be played and display the first frame
+//      autoInitialize: true,
+//      looping: widget.looping,
+//      //fullScreenByDefault: true,
+//      //autoPlay: true,
+//      allowFullScreen: true,
+//
+//      showControls: true,
+//      deviceOrientationsAfterFullScreen: [
+//        //DeviceOrientation.landscapeRight,
+//        //DeviceOrientation.landscapeLeft,
+//        DeviceOrientation.portraitUp,
+//        DeviceOrientation.portraitDown,
+//      ],
+//      // Errors can occur for example when trying to play a video
+//      // from a non-existent URL
+//      errorBuilder: (context, errorMessage) {
+//        return Center(
+//          child: Text(
+//            errorMessage,
+//            style: TextStyle(color: Colors.white),
+//          ),
+//        );
+//      },
+//    );
+
+
+
+
+
     _chewieController = ChewieController(
-      videoPlayerController: widget.videoPlayerController,
-      aspectRatio: 16 / 9,
-      // Prepare the video to be played and display the first frame
-      autoInitialize: true,
-      looping: widget.looping,
-      //fullScreenByDefault: true,
-      autoPlay: true,
-      deviceOrientationsAfterFullScreen: [DeviceOrientation.portraitUp],
-      // Errors can occur for example when trying to play a video
-      // from a non-existent URL
-      errorBuilder: (context, errorMessage) {
-        return Center(
-          child: Text(
-            errorMessage,
-            style: TextStyle(color: Colors.white),
-          ),
-        );
-      },
+        videoPlayerController: widget.videoPlayerController,
+        aspectRatio: 9 / 16,
+        autoPlay: true,
+        fullScreenByDefault: false,
+        looping: true,
+        deviceOrientationsAfterFullScreen: [
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown
+        ],
+//        placeholder: new Center(
+//        //  child: RVTApp.BuildActivityIndicator(context),
+//        ),
+        routePageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondAnimation, provider) {
+          return AnimatedBuilder(
+            animation: animation,
+            builder: (BuildContext context, Widget child) {
+              return VideoScaffold(
+                child: Scaffold(
+                  resizeToAvoidBottomPadding: false,
+                  body: Container(
+                    alignment: Alignment.center,
+                    color: Colors.black,
+                    child: provider,
+                  ),
+                ),
+              );
+            },
+          );
+        }
+    );
+
+
+
+
+    _chewieController.addListener(() {
+      if (_chewieController.isFullScreen) {
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown,
+        ]);
+      } else {
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown,
+        ]);
+      }
+    }
     );
   }
 
@@ -141,3 +203,44 @@ class _ChewieListItemState extends State<ChewieListItem> {
     _chewieController.dispose();
   }
 }
+class VideoScaffold extends StatefulWidget {
+  const VideoScaffold({Key key, this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  State<StatefulWidget> createState() => _VideoScaffoldState();
+}
+
+class _VideoScaffoldState extends State<VideoScaffold> {
+
+  @override
+  void initState() {
+    SystemChrome.setPreferredOrientations([
+     // DeviceOrientation.portraitUp,
+     // DeviceOrientation.portraitDown,
+    ]);
+   // AutoOrientation.portraitMode();
+    super.initState();
+  }
+
+  @override
+  dispose(){
+    SystemChrome.setPreferredOrientations([
+    //  DeviceOrientation.portraitUp,
+     // DeviceOrientation.portraitDown,
+    ]);
+    //AutoOrientation.portraitMode();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return null;
+//  }
